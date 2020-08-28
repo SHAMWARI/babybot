18edd1eb1ce310be7e08faf879e09bace2b44865
SHAMWARI <70348991+SHAMWARI@users.noreply.github.com>
2020-08-28 23:36:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -93,9 +93,10 @@ async def mute(ctx,member:discord.Member):
 	emb = discord.Embed(title='Мут', description='Мут участника', color=0xeeff00)
 	await ctx.channel.purge(limit=1)
 	mute_role=discord.itils.get(ctx.message.guild.roles, name='mute')
+	await member.add_roles(mute_role)
 	emb.set_author(name=member.name, icon_url=member.avatar_url)
 	emb.add_field(name='Замьючен участник',value='Замьючен участник:{}'.format(member.mention))
-	await member.add_roles(mute_role,embed=emb)
+	await ctx.send(embed=emb)
 
 #send_message_member
 @client.command()
