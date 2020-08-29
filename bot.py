18105132af8ae80e2d9ef0443d5ebaa42aad9591
SHAMWARI <70348991+SHAMWARI@users.noreply.github.com>
2020-08-29 15:17:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -101,60 +101,6 @@ async def mute(ctx,member:discord.Member):
 	
 	await ctx.send(embed=emb)
 
-#play
-#@client.command()
-#async def play(ctx, url:str):
-#########################	global voice
-########################	channel=ctx.message.author.voice.channel
-#######################	voice=get(client.voice_clients,guild=ctx.guild)
-######################	if voice and voice.is_connected():
-#####################		await voice.move_to(channel)
-####################	else:
-###################		voice = await channel.connect()
-##################		await ctx.send(f'Бот присоединился к каналу: **{channel}**')
-#################	song_there = os.path.isfile('song.mp3')
-################	try:
-###############		if song_there:
-##############			os.remove('song.mp3')
-#############			print('[log] Старый файл удален')
-############	except PermissionError:
-###########		print('[log] Не удалось удалить файл')
-##########	await ctx.send('Пожалуйста ожидайте')
-#########	voice=get(client.voice_clients,guild=ctx.guild)
-########	ydl_opts={
-#######		'format':'bestaudio/best',
-######		'postprocessors':[{
-####			'key': 'FFmpegExtractAudio',
-###			'preferredcodec':'mp3',
-##			'preferredquality':'192'
-##		}],
-#	}
-#	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#		print('[log]Загружаю музыку...')
-#		ydl.download([url])
-#	for file in os.listdir('./'):
-##########		if file.endswith('.mp3'):
-#########			name=file
-########			print('[log] Переименовываю файл: {file}')
-#######			os.rename(file,'song.mp3')
-######
-#####	voice.play(discord.FFmpegPCMAudio('song.mp3'),after=lambda e:print(f'[log]{name}, **__Музыка закончила свое проигрывание__**'))
-#####	voice.source=discord.PCMVolumeTransformer(voice.source)
-####	voice.source.volume = 1.05
-###	song_name=name.rsplit('-',2)
-##	await ctx.send(f'Сейчас проигрывает музыка: **{song_name[0]}**')
-#
-#leave
-#########@client.command()
-########async def leave(ctx):
-#######	channel=ctx.message.author.voice.channel
-######	voice=get(client.voice_clients,guild=ctx.guild)
-#####	if voice and voice.is_connected():
-####		await voice.disconnect()
-###	else:
-##		voice = await disconnect.channel()
-#		await ctx.send(f'Бот отсоединился от канала: #{channel}')
-
 #send_message_member
 @client.command()
 async def send_message_member(ctx, member:discord.Member):
