9cb63f9da01da8d25e21dfee8d1e12c99ffa2b07
SHAMWARI <70348991+SHAMWARI@users.noreply.github.com>
2020-08-29 13:32:27 +0300

    Update self_bot.py

--- self_bot.py
+++ self_bot.py
@@ -2,19 +2,12 @@ import discord
 from discord.ext import commands
 import os 
 
-client = commands.Bot(command_prefix = '>', self_bot=False)
+client = commands.Bot(command_prefix = 'PREFIX', self_bot=False)
 
 @client.event
 async def on_ready():
 	print('log')
 
-
 @client.command()
 async def say(ctx,*,text):
 	await ctx.send(embed=discord.Embed(description=text))
-
-token = os.environ.get('TOKENBOT')
-token2 = os.environ.get('TOKENBOT2')
-
-client.run("TOKENBOT2", bot=True)
-client.run(str(token))
