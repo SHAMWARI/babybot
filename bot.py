ba91d59a9b7aaa100fef5bea9f3e942683504c51
SHAMWARI <70348991+SHAMWARI@users.noreply.github.com>
2020-08-29 15:43:50 +0300

    all

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
+from discord import utils
 import os 
+import config
 
 PREFIX='>'
 
@@ -86,6 +88,32 @@ async def help(ctx):
 	emb.add_field(name='{}mute'.format(PREFIX), value='Мут участника',inline=False)
 	await ctx.send(embed=emb)
 
+#auto_role
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+ 
+    async def on_raw_reaction_add(self, payload):
+        if payload.message_id == config.POST_ID:
+            channel = self.get_channel(payload.channel_id) 
+            message = await channel.fetch_message(payload.message_id) 
+            member = utils.get(message.guild.members, id=payload.user_id) 
+ 
+            try:
+                emoji = str(payload.emoji) 
+                role = utils.get(message.guild.roles, id=config.ROLES[emoji]) 
+            
+                if(len([i for i in member.roles if i.id not in config.EXCROLES]) <= config.MAX_ROLES_PER_USER):
+                    await member.add_roles(role)
+                    print('[Успешно]  {0.display_name} получил роль {1.name}'.format(member, role))
+                else:
+                    await message.remove_reaction(payload.emoji, member)
+                    print('[Ошибка] Too many roles for user {0.display_name}'.format(member))
+            
+            except KeyError as e:
+                print('[Ошибка] KeyError, no role found for ' + emoji)
+            except Exception as e:
+                print(repr(e))
 #mute
 @client.command()
 @commands.has_permissions(administrator=True)
