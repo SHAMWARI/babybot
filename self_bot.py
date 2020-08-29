5e11c780c5493894c320c53ebfddb9f6d99dc9f8
SHAMWARI <70348991+SHAMWARI@users.noreply.github.com>
2020-08-29 13:05:58 +0300

    Create self_bot.py

---
+++ self_bot.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import
+
+client = commands.Bot(command_prefix = 'PREFIX', self_bot=False)
+
+@client.event
+async def on_ready():
+	print('log')
+
+
+@client.command()
+async def say(ctx,*,text):
+	await ctx.send(embed=discord.Embed(description=text))
+
+client.run('TOKENBOT2',bot=True)
