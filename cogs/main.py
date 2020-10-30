ce5c92d5ea09704e8edb8cd42061a3f31f203371
SHAMWARI <70348991+SHAMWARI@users.noreply.github.com>
2020-10-30 20:47:04 +0200

    fdsf
    dsfdsf

---
+++ cogs/main.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+class User(commans.Cog):
+
+	def __init__(self, client):
+		self.client = client
+
+	@client.Cog.listener()
+	async def on_ready(self):
+		print("Bot ready")
+
+	@commands.command(aliases = ["user-info", "Информация"])
+	async def info(self, ctx):
+		await ctx.send("Hello, World!")
+
+def setup(client):
+	client.add_cog(User(client))
