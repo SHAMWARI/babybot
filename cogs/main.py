f93c402234a95d34f78c02b7accdb3b96c020a45
SHAMWARI <70348991+SHAMWARI@users.noreply.github.com>
2020-11-22 21:49:52 +0200

    Update main.py

--- cogs/main.py
+++ cogs/main.py
@@ -11,25 +11,11 @@ class main(commands.Cog):
 	async def help (self, ctx):
 		await ctx.channel.purge (limit = 1)
 		emb = discord.Embed (title = 'Навигация по командам', color = 0xeeff00)
-		emb.add_field (name = 'clear', value = 'Очистка чата', inline = False)
-		emb.add_field (name = 'kick',  value = 'Кик участника', inline = False)
-		emb.add_field (name = 'ban',  value = 'Бан участника', inline = False)
-		emb.add_field (name = 'unban',  value = 'Разбан участника', inline = False)
+		emb.add_field (name = 'clear', value = 'Очистка чата', inline = True)
+		emb.add_field (name = 'kick',  value = 'Кик участника', inline = True)
+		emb.add_field (name = 'ban',  value = 'Бан участника', inline = True)
+		emb.add_field (name = 'unban',  value = 'Разбан участника', inline = True)
 		await ctx.send (embed = emb)
-
-#send_message_member
-	@commands.command()
-	@commands.has_permissions(administrator = True)
-	async def say(self, ctx, user_id = None, *, args = None):
-		if user_id !=  None and args !=  None:
-			try:
-				target = await client.fetch_user(user_id)
-				await target.send(args)
-				await ctx.channel.send("'" + args + "' Сообщение было отправлено: " + target.name)
-			except:
-				await ctx.channel.send("Не удалось установить dm для данного пользователя.")
-		else:
-			await ctx.channel.send('Укажите сообщение')
 
 def setup(client):
 	client.add_cog(main(client))
