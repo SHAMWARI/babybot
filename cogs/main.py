2e9e8e28c6e7b03421a2c7512d1d865f6c9f6d23
SHAMWARI <70348991+SHAMWARI@users.noreply.github.com>
2020-11-27 12:29:17 +0200

    Update main.py

--- cogs/main.py
+++ cogs/main.py
@@ -11,10 +11,10 @@ class main(commands.Cog):
 	async def help (self, ctx):
 		await ctx.channel.purge (limit = 1)
 		emb = discord.Embed (title = 'Навигация по командам', color = 0xeeff00)
-		emb.add_field (name = 'clear', value = 'Очистка чата', inline = True)
-		emb.add_field (name = 'kick',  value = 'Кик участника', inline = True)
-		emb.add_field (name = 'ban',  value = 'Бан участника', inline = True)
-		emb.add_field (name = 'unban',  value = 'Разбан участника', inline = True)
+		emb.add_field (name = 'clear', value = 'Очистка чата', inline = False)
+		emb.add_field (name = 'kick',  value = 'Кик участника', inline = False)
+		emb.add_field (name = 'ban',  value = 'Бан участника', inline = False)
+		emb.add_field (name = 'unban',  value = 'Разбан участника', inline = False)
 		await ctx.send (embed = emb)
 
 def setup(client):
