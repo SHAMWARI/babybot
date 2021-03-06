0979af4edde95caf60684aa042df924e3c1054d6
SHAMWARI <dima_dima_zhuko@mail.ua>
2021-03-07 00:22:31 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -95,7 +95,7 @@ async def clear(ctx, amount: int):
 @client.command()
 async def kick(ctx, member: discord.Member,  *,  reason = None):
 	emb = discord.Embed(
-		title='🤡', description='Кикнут участник:' +  member.mention,
+		title='🤡', description='Кикнут участник: ' + member.mention,
 			color=0xff0000)
 	await ctx.channel.purge(limit = 1)
 	await member.kick(reason = reason)
@@ -107,7 +107,7 @@ async def kick(ctx, member: discord.Member,  *,  reason = None):
 async def ban(ctx, member: discord.Member,  *,  reason = None):
 	await ctx.channel.purge(limit=1)
 	emb = discord.Embed(
-		title='🤡', description='Забанен участник:' + member.mention, 
+		title='🤡', description='Забанен участник: ' + member.mention, 
 			color=0xeeff00)
 	await member.ban(reason = reason)
 	emb.set_author(name = member.name,  icon_url = member.avatar_url)
@@ -118,7 +118,7 @@ async def ban(ctx, member: discord.Member,  *,  reason = None):
 async def unban(ctx, *, member: discord.Member):
 	await ctx.channel.purge(limit = 1)
 	emb = discord.Embed(
-		title='👋', description='Разбанен участник:' + member.mention,  
+		title='👋', description='Разбанен участник: ' + member.mention,  
 			color=0xeeff00)
 	banned_users =  await ctx.guild.bans()
 	for ban_entry in banned_users:
